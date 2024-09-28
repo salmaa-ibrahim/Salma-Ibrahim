507c92d999116f4b38055dac0eb2e150fa3d106f
salmaa-ibrahim <salmaa.ibrahim19@gmail.com>
2024-09-28 17:29:05 +0300

    add cv

--- Salma Ibrahim/src/pages/home/home.jsx
+++ Salma Ibrahim/src/pages/home/home.jsx
@@ -25,7 +25,7 @@ function Home() {
                     <h1 className={passevedValues.theme + "Name"}>Salma Ibrahim</h1>
                     <h3 className={passevedValues.theme + "HomeTxt"} id="position">Frontend Developer (React.js)</h3>
                     <div className="homeButtons">
-                        <a className={passevedValues.theme + "CvBtn"} href="https://drive.google.com/file/d/1E-mtkQHE6VRGGBT_N_hHdtgB30iib_Vc/view">Look at my CV</a>
+                        <a className={passevedValues.theme + "CvBtn"} href="https://drive.google.com/file/d/1uh_NZE34ipmZLE2ob52D7MF8_Q4hbUFM/view?usp=drive_link">Look at my CV</a>
                         <a className={passevedValues.theme + "ContactBtn"} href="#contact">Contact me</a>
                     </div>
                     <div className="HomeIcons">
