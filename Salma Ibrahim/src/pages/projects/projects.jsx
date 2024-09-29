be1bd726bf89a9a926d83297fc09488de8e9fe9f
salmaa-ibrahim <salmaa.ibrahim19@gmail.com>
2024-09-29 17:07:55 +0300

    projects page

--- Salma Ibrahim/src/pages/projects/projects.jsx
+++ Salma Ibrahim/src/pages/projects/projects.jsx
@@ -1,7 +1,7 @@
 import "./projects.css"
 import { useContext } from "react";
 import { ThemeContext } from '../../App'
-import ProjectOneImg from "../../../public/images/ProjectPage/Github Search.png"
+import ProjectOneImg from "../../../public/images/ProjectPage/Github.png"
 import ReactImg from "../../../public/images/ProjectPage/react.svg"
 import ViteImg from "../../../public/images/ProjectPage/vitejs.svg"
 function Projects() {
@@ -9,7 +9,7 @@ function Projects() {
     const Projects = [
         {
             image: ProjectOneImg,
-            name: "project one",
+            name: "User Search Github",
             Img1:ReactImg ,
             Img2:ViteImg ,
         },
@@ -27,7 +27,6 @@ function Projects() {
                     <label>Browse My Recent</label>
                     <h1 className={passevedValues.theme + "AboutLable"}>Projects</h1>
                 </div>
-            </div>
             <div className="projectsContent">
                 {Projects.map((project) => {
                     return (
@@ -37,19 +36,23 @@ function Projects() {
                                     <img src={project.image} alt="#" />
                                 </div>
                                 <div className="projectTools">
-                                    <p className={passevedValues.theme + "ProjectName"}>{project.name}</p>
+                                    <h2 className={passevedValues.theme + "ProjectName"}>{project.name}</h2>
                                     <div className="Tools">
-                                        <img src={project.Img1} alt="#" /> <img src={project.Img2} alt="#" />
+                                        <img src={project.Img1} alt="#" /> 
+                                        <img src={project.Img2} alt="#" />
+                                        <img src={project.Img1} alt="#" /> 
+                                        <img src={project.Img2} alt="#" />
                                     </div>
                                 </div>
                                 <div className="ProjectDisplay">
-                                    <button className={passevedValues.theme + "ProjectDisplayBtn"}> Github</button>
-                                    <button className={passevedValues.theme + "ProjectDisplayBtn"}> Live Demo</button>
+                                    <a href="#" className={passevedValues.theme + "ProjectDisplayBtn"}> Github</a>
+                                    <a href="#" className={passevedValues.theme + "ProjectDisplayBtn"}> Live Demo</a>
                                 </div>
                             </div>
                         </>
                     )
                 })}
+            </div>
             </div>
         </>
     )
