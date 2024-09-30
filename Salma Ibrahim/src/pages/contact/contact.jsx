5b74647e23f8a3e6525b1f572a557902da918e15
salmaa-ibrahim <salmaa.ibrahim19@gmail.com>
2024-09-30 14:04:15 +0300

    add progects

--- Salma Ibrahim/src/pages/contact/contact.jsx
+++ Salma Ibrahim/src/pages/contact/contact.jsx
@@ -18,36 +18,42 @@ function Contact() {
     const passevedValues = useContext(ThemeContext)
     const ContactInfo = [
         {
+        id:0,
         Image: EmailWhite,
         Image2: EmailBlack,
         lable: "salmaa.ibrahimm19@gmail.com",
         link: "mailto:salmaa.ibrahimm19@gmail.com"
     },
     {
+        id:1,
         Image: WhatsAppWhite,
         Image2: WhatsAppBlack,
         lable: "WhatsApp-Salma Ibrahim",
         link: "https://wa.link/vylygp"
     },
     {
+        id:2,
         Image: PhoneWhite,
         Image2: PhoneBlack,
         lable: "01203527773",
         link: "tel:01203527773"
     },
     {
+        id:3,
         Image: LinkedInWhite,
         Image2: LinkedInBlack,
         lable: "linkedin.com/in/salma-ibrahim",
         link: "https://www.linkedin.com/in/salma-ibrahim-07116a287"
     },
     {
+        id:4,
         Image: GithubWhite,
         Image2: GithubBlack,
         lable: "salmaa-ibrahim",
         link: "https://github.com/salmaa-ibrahim"
     },
     {
+        id:5,
         Image: VercelWhite,
         Image2: VercelBlack,
         lable: "salma-ibrahims-projects",
@@ -67,7 +73,7 @@ function Contact() {
                     {ContactInfo.map((item) => {
                         return (
                             <>
-                                <div className="contactLink">
+                                <div className="contactLink" key={item.id}>
                                     <img src={item.Image} alt="" className={passevedValues.theme +"Sun"} />
                                     <img src={item.Image2} alt="" className={passevedValues.theme +"Moon"} />
                                     <a className={passevedValues.theme +"ContactLink"}  href={item.link}>{item.lable}</a>
--- Salma Ibrahim/src/pages/projects/projects.jsx
+++ Salma Ibrahim/src/pages/projects/projects.jsx
@@ -1,24 +1,98 @@
 import "./projects.css"
 import { useContext } from "react";
 import { ThemeContext } from '../../App'
-import ProjectOneImg from "../../../public/images/ProjectPage/Github.png"
+import JopListing from "../../../public/images/ProjectPage/JopListing.png"
+import TimeTracking from "../../../public/images/ProjectPage/TimeTracking.png"
+import EcommerceWebsite from "../../../public/images/ProjectPage/EcommerceWebsite.png"
+import ProfileCard from "../../../public/images/ProjectPage/ProfileCard.png"
+import SocialLink from "../../../public/images/ProjectPage/SocialLink.png"
+import ApplicationForm from "../../../public/images/ProjectPage/ApplicationForm.png"
+import GithubUser from "../../../public/images/ProjectPage/GithubUser.png"
+import HTMLImg from "../../../public/images/ProjectPage/html.svg"
+import CSSImg from "../../../public/images/ProjectPage/css.svg"
 import ReactImg from "../../../public/images/ProjectPage/react.svg"
 import ViteImg from "../../../public/images/ProjectPage/vitejs.svg"
 function Projects() {
     const passevedValues = useContext(ThemeContext)
     const Projects = [
         {
-            image: ProjectOneImg,
-            name: "User Search Github",
-            Img1:ReactImg ,
-            Img2:ViteImg ,
+            id : 0 ,
+            image: TimeTracking,
+            name: "Time Tracking ",
+            Img1:HTMLImg ,
+            Img2:CSSImg ,
+            Img3:ReactImg ,
+            Img4:ViteImg ,
+            github: "https://github.com/salmaa-ibrahim/Time-Tracking",
+            liveDemo:"https://time-tracking-ashy-nine.vercel.app/"
         },
         {
-            image:ProjectOneImg ,
-            name: "project Two",
-            Img1:ReactImg ,
-            Img2:ViteImg ,
+            id : 1 ,
+            image: EcommerceWebsite,
+            name: "Ecommerce Website",
+            Img1:HTMLImg ,
+            Img2:CSSImg ,
+            Img3:ReactImg ,
+            Img4:ViteImg ,
+            github: "https://github.com/salmaa-ibrahim/final-task",
+            liveDemo:"https://final-task-azure.vercel.app/"
         },
+        {
+            id : 2 ,
+            image: JopListing,
+            name: "Jop Listing",
+            Img1:HTMLImg ,
+            Img2:CSSImg ,
+            Img3:ReactImg ,
+            Img4:ViteImg ,
+            github: "https://github.com/salmaa-ibrahim/Job-Listings",
+            liveDemo:"https://job-listings-eta-nine.vercel.app/"
+        },
+        {
+            id : 3 ,
+            image: ApplicationForm,
+            name: "Application Form",
+            Img1:HTMLImg ,
+            Img2:CSSImg ,
+            Img3:ReactImg ,
+            Img4:ViteImg ,
+            github: "https://github.com/salmaa-ibrahim/Form",
+            liveDemo:"https://application-form-ecru.vercel.app/"
+        },
+        {
+            id : 4 ,
+            image: SocialLink,
+            name: "SocialLink",
+            Img1:HTMLImg ,
+            Img2:CSSImg ,
+            Img3:ReactImg ,
+            Img4:ViteImg ,
+            github: "https://github.com/salmaa-ibrahim/Social-Links",
+            liveDemo:"https://social-links-gray.vercel.app/"
+        },
+        {
+            id : 5 ,
+            image: GithubUser,
+            name: "User Search Github",
+            Img1:HTMLImg ,
+            Img2:CSSImg ,
+            Img3:ReactImg ,
+            Img4:ViteImg ,
+            github: "https://github.com/salmaa-ibrahim/Search-Github",
+            liveDemo:"https://github-user-search-tau-nine.vercel.app/"
+        },
+        {
+            id : 6 ,
+            image: ProfileCard,
+            name: "Profile Card",
+            Img1:HTMLImg ,
+            Img2:CSSImg ,
+            Img3:ReactImg ,
+            Img4:ViteImg ,
+            github: "https://github.com/salmaa-ibrahim/Profile-Card",
+            liveDemo:"https://profile-card-one-chi.vercel.app/"
+        },
+        
     ]
     return (
         <>
@@ -31,22 +105,22 @@ function Projects() {
                 {Projects.map((project) => {
                     return (
                         <>
-                            <div className="projectDiv">
+                            <div className="projectDiv" key={project.id}>
                                 <div className="projectImg">
                                     <img src={project.image} alt="#" />
                                 </div>
                                 <div className="projectTools">
                                     <h2 className={passevedValues.theme + "ProjectName"}>{project.name}</h2>
                                     <div className="Tools">
-                                        <img src={project.Img1} alt="#" /> 
-                                        <img src={project.Img2} alt="#" />
+                                        <img src={project.Img3} alt="#" /> 
+                                        <img src={project.Img4} alt="#" />
                                         <img src={project.Img1} alt="#" /> 
                                         <img src={project.Img2} alt="#" />
                                     </div>
                                 </div>
                                 <div className="ProjectDisplay">
-                                    <a href="#" className={passevedValues.theme + "ProjectDisplayBtn"}> Github</a>
-                                    <a href="#" className={passevedValues.theme + "ProjectDisplayBtn"}> Live Demo</a>
+                                    <a href={project.github} className={passevedValues.theme + "ProjectDisplayBtn"}> Github</a>
+                                    <a href={project.liveDemo} className={passevedValues.theme + "ProjectDisplayBtn"}> Live Demo</a>
                                 </div>
                             </div>
                         </>
