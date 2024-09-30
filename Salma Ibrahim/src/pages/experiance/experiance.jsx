6d66003e4d86c8deb4bbdf49e5538c75769b000d
salmaa-ibrahim <salmaa.ibrahim19@gmail.com>
2024-09-30 17:07:42 +0300

    updates

--- Salma Ibrahim/src/pages/experiance/experiance.jsx
+++ Salma Ibrahim/src/pages/experiance/experiance.jsx
@@ -1,13 +1,24 @@
 import "./experince.css"
 import { useContext, useState } from "react";
 import { ThemeContext } from '../../App'
-import HTML from "../../../public/images/HTML.svg"
-import CSS from "../../../public/images/css.svg"
-import JS from "../../../public/images/javascript.svg"
-import React from "../../../public/images/react.svg"
-import Keyboard from "../../../public/images/keyboard.jpeg"
-import Bootstrab from "../../../public/images/bootstrap.svg"
-// import ChatGPT from "../../../public/images/chatgpt.svg"
+import HTML from "../../../public/images/ExperincePage/HTML.svg"
+import CSS from "../../../public/images/ExperincePage/css.svg"
+import JS from "../../../public/images/ExperincePage/javascript.svg"
+import React from "../../../public/images/ExperincePage/react.svg"
+import Bootstrab from "../../../public/images/ExperincePage/bootstrap.svg"
+import WebFundamental from "../../../public/images/ExperincePage/web fundamentl.svg"
+import APIs from "../../../public/images/ExperincePage/API.svg"
+import JSON from "../../../public/images/ExperincePage/JSON.svg"
+import CleanCode from "../../../public/images/ExperincePage/clean code.svg"
+import GraphicDesign from "../../../public/images/ExperincePage/graphic design.svg"
+import ProblemSolving from "../../../public/images/ExperincePage/problem solving.svg"
+import Networks from "../../../public/images/ExperincePage/network.svg"
+import Cpp from "../../../public/images/ExperincePage/Cpp.svg"
+import WrittingSpeed from "../../../public/images/ExperincePage/keyboard.jpeg"
+import VSC from "../../../public/images/ExperincePage/VSC.svg"
+import Figma from "../../../public/images/ExperincePage/figma.svg"
+import MSOffice from "../../../public/images/ExperincePage/microsoft-office.svg"
+import Certificate from "../../../public/images/ExperincePage/certificate.jpeg"
 
 function Experiance() {
     const passevedValues = useContext(ThemeContext)
@@ -28,41 +39,120 @@ function Experiance() {
     }
 
     const Tools = [{
+        id : 0 ,
         image: React,
         title: "React",
         level: "Advanced"
     },
     {
+        id : 1 ,
         image: HTML,
         title: "HTML",
         level: "Experienced"
     },
     {
+        id : 2 ,
         image: CSS,
         title: "Css",
         level: "Experienced"
     },
     {
-        image: Bootstrab,
-        title: "Bootstrab",
-        level: "Basics"
-    },{
+        id : 3 ,
         image: JS,
         title: "JavaScript",
         level: "Intermediate"
-    },{
-        image: Keyboard,
+    },
+    {
+        id : 4 ,
+        image: Bootstrab,
+        title: "Bootstrab",
+        level: "Basics"
+    },
+    {
+        id : 5 ,
+        image: WebFundamental,
+        title: "Web Fundamentals",
+        level: "Experienced"
+    },
+    {
+        id :6  ,
+        image: APIs,
+        title: "APIs",
+        level: "Experienced"
+    },
+    {
+        id :7  ,
+        image: JSON,
+        title: "JSON",
+        level: "Experienced"
+    },
+    {
+        id : 8 ,
+        image: WrittingSpeed,
         title: "Writing Speed",
+        level: "Experienced"
+    },
+    {
+        id : 9 ,
+        image: CleanCode,
+        title: "Clean Code",
+        level: "Experienced"
+    },
+    {
+        id : 10 ,
+        image: GraphicDesign,
+        title: "Graphic Design",
+        level: "Experienced"
+    },
+    {
+        id :  11,
+        image: ProblemSolving,
+        title: "Problem Solving",
+        level: "Experienced"
+    },
+    {
+        id : 12 ,
+        image: Networks,
+        title: "Networks",
+        level: "Experienced"
+    },
+    {
+        id : 13  ,
+        image: Cpp,
+        title: "C++",
+        level: "Experienced"
+    },
+    {
+        id : 14  ,
+        image: VSC,
+        title: "VSC" ,
+        level: "Experienced"
+    },
+    {
+        id : 15  ,
+        image: Figma,
+        title: "Figm" ,
+        level: "Experienced"
+    },
+    {
+        id : 16  ,
+        image: MSOffice,
+        title: "MS Office" ,
         level: "Experienced"
     },
     ]
 
-    const Skills = [{
-        image: HTML,
-        title: "HTML",
-        level: "Advanced"
+    const Skills = [
+        {
+        image: Certificate,
+        title: "Frontend Development Basic Level",
+        // level: "Advanced"
     },
-
+    {
+        image: Certificate,
+        title: "Frontend Development Advanced Level",
+        // level: "Advanced"
+    },
     ]
 
     return (
@@ -75,7 +165,7 @@ function Experiance() {
                 <div className="ExperinceContent">
                     <div className={passevedValues.theme +"ExperinceTitle"}>
                         <div id="TitleDiv" className={select ? "notSelected" : " selected"} onClick={Unselect}>
-                            <button className={passevedValues.theme + "TitleButton"} onClick={closeSkills}>Skills</button>
+                            <button className={passevedValues.theme + "TitleButton"} onClick={closeSkills}>Skills and Tools</button>
                         </div>
                         <div id="TitleDiv" className={select ? "selected" : "notSelected"} onClick={Select}>
                             <button className={passevedValues.theme + "TitleButton"} onClick={openSkills} >Courses</button>
@@ -85,7 +175,7 @@ function Experiance() {
                         {Tools.map((prop) => {
                             return (
                                 <>
-                                    <div className={active ? passevedValues.theme + "OpenSkill" : passevedValues.theme + "CloseSkill"}>
+                                    <div className={active ? passevedValues.theme + "OpenSkill" : passevedValues.theme + "CloseSkill"} key={prop.id}>
                                         <div className="ExperinceIconDiv">
                                             <img className="IconImg" src={prop.image} alt="" />
                                             <div className="EXPIcon">
@@ -101,10 +191,10 @@ function Experiance() {
                             return (
                                 <>
                                     <div className={active ? passevedValues.theme + "CloseSkill" : passevedValues.theme + "OpenSkill"} >
-                                        <div className="ExperinceIconDiv">
-                                            <img className="IconImg" src={skill.image} alt="" />
+                                        <div className="CoursesIconDiv">
+                                            <img className="courseImg" src={skill.image} alt="" />
+                                            <h3 className={passevedValues.theme + "CourseName"}>{skill.title}</h3>
                                             <div className="EXPIcon">
-                                                <h3 className={passevedValues.theme + "IconLable"}>{skill.title}</h3>
                                                 <span className={passevedValues.theme + "IconLable2"}>{skill.level}</span>
                                             </div>
                                         </div>
